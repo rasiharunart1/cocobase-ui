a10f651a4f282dc428284d338a673badf917f563
rasiharunart1 <2211102080@ittelkom-pwt.ac.id>
2026-01-31 00:10:36 +0700

    fix: correct globals.css import path in petani layout

--- app/(petani)/layout.tsx
+++ app/(petani)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../../globals.css";
+import "../globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
